7f3e6bee1b4f6c973c7ccec04246cd40ddd74f20
Tri Nguyen <tri.nguyen@x-factory.jp>
2023-12-20 10:49:02 +0700

    feat: implement typeorm

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { UserModule } from '@/modules/user/user.module';
+import { getConnectionConfig } from '@/ormconfig';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...getConnectionConfig(),
+        autoLoadEntities: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
