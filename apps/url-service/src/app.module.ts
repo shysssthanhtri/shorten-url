6b0cc135dbe1432e4e097b3d861927f105102f65
Tri Nguyen <tri.nguyen@x-factory.jp>
2023-12-20 15:58:19 +0700

    feat: url-service implement url.module

--- apps/url-service/src/app.module.ts
+++ apps/url-service/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UrlModule } from '@/modules/url/url.module';
+import { getConnectionConfig } from '@/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    UrlModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...getConnectionConfig(),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
