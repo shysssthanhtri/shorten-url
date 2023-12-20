8743def3324ef784717f44fb4e65e4d22cf7a500
Tri Nguyen <tri.nguyen@x-factory.jp>
2023-12-20 09:39:57 +0700

    chore: listening

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from '@/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, () => {
-    console.log('Listening: ...');
+    console.log('Listening: ......');
   });
 }
 bootstrap();
